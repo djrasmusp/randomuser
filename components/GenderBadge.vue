4fb5894b54f174b3a7483a0e05e47ae15d4ee7bc
Rasmus P <hello@rasmusp.com>
2023-04-23 01:12:36 +0200

    testing with scoped styling

--- components/GenderBadge.vue
+++ components/GenderBadge.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="group" :class="gender">
     <div
-      :class="[isBig ? 'bottom-4 right-4 gap-x-2 px-2 py-1 rounded-lg' : 'bottom-2 right-2 gap-x-1.5 px-1.5 py-1 rounded-lg', hideOnMobile ? 'hidden lg:flex' : 'flex']"
-      class="absolute items-center bg-gradient-to-t drop-shadow-sm group-[.male]:from-blue-100 group-[.female]:from-rose-100 group-[.male]:to-blue-50 group-[.female]:to-rose-50 group-[.male]:fill-blue-800 group-[.female]:fill-rose-800 group-[.male]:text-blue-900 group-[.female]/badge:text-rose-900">
+      :class="[isBig ? 'bottom-4 right-4 gap-x-2 px-2 py-1 rounded-lg' : 'bottom-2 right-2 gap-x-1.5 px-1.5 py-1 rounded-lg', hideOnMobile ? 'hidden lg:flex' : 'flex', gender]"
+      class="absolute items-center drop-shadow-sm">
       <inline-svg :class="[isBig ? 'h-4 w-4' : 'h-3 w-3']" :src="genderIcon" :title="gender" aria-hidden="true"
                   class="h-3 w-3"></inline-svg>
       <span :class="[isBig ? '' : 'text-xs']" class="font-medium capitalize">{{ gender }}</span>
     </div>
-  </div>
 </template>
 
 <script>
@@ -20,13 +18,6 @@ export default {
 
   components: {
     InlineSvg,
-  },
-
-  data() {
-    return {
-      female: "from-rose-100 to-rose-50 text-rose-800 fill-rose-800",
-      male: 'from-blue-100 to-blue-50 text-blue-800 fill-blue-800'
-    }
   },
 
   computed: {
@@ -50,3 +41,13 @@ export default {
 
 }
 </script>
+
+<style scoped>
+.male {
+  @apply bg-gradient-to-t from-blue-100 to-blue-50 text-blue-800 fill-blue-800;
+}
+
+.female {
+  @apply bg-gradient-to-t from-rose-100 to-rose-50 text-rose-800 fill-rose-800;
+}
+</style>
--- components/Navigation.vue
+++ components/Navigation.vue
@@ -50,7 +50,3 @@ export default Vue.extend({
   }
 })
 </script>
-
-<style scoped>
-
-</style>
