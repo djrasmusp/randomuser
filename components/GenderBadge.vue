3dfd908d997771e94114ffd26908b83194babb65
Rasmus P <hello@rasmusp.com>
2023-04-23 01:07:43 +0200

    testing bg-gradient

--- components/GenderBadge.vue
+++ components/GenderBadge.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="group/badge" :class="gender">
+  <div class="group" :class="gender">
     <div
       :class="[isBig ? 'bottom-4 right-4 gap-x-2 px-2 py-1 rounded-lg' : 'bottom-2 right-2 gap-x-1.5 px-1.5 py-1 rounded-lg', hideOnMobile ? 'hidden lg:flex' : 'flex']"
-      class="items-center bg-gradient-to-t absolute drop-shadow-sm group-[.male]/badge:from-blue-100 group-[.male]/badge:to-blue-50 group-[.male]/badge:text-blue-900 group-[.male]/badge:fill-blue-800 group-[.female]/badge:from-rose-100 group-[.female]/badge:to-rose-50 group-[.female]/badge:text-rose-900 group-[.female]/badge:fill-rose-800">
+      class="absolute items-center bg-gradient-to-t drop-shadow-sm group-[.male]:from-blue-100 group-[.female]:from-rose-100 group-[.male]:to-blue-50 group-[.female]:to-rose-50 group-[.male]:fill-blue-800 group-[.female]:fill-rose-800 group-[.male]:text-blue-900 group-[.female]/badge:text-rose-900">
       <inline-svg :class="[isBig ? 'h-4 w-4' : 'h-3 w-3']" :src="genderIcon" :title="gender" aria-hidden="true"
                   class="h-3 w-3"></inline-svg>
       <span :class="[isBig ? '' : 'text-xs']" class="font-medium capitalize">{{ gender }}</span>
